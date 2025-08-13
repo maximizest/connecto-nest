f1a65b10473872b973ea60bed3b2cf4a3f5f08b3
dev-jwshin <dev@foryourdev.com>
2025-08-13 10:58:48 +0900

    행성 모듈에서 접근 권한 관련 Guard 제거: PlanetModule에서 PlanetAccessGuard, DirectPlanetPermissionGuard, TimeRestrictionGuard를 제거하여 코드 간결성을 높임. 불필요한 권한 체크 로직을 정리하여 가독성을 개선함.

--- src/modules/planet/planet.module.ts
+++ src/modules/planet/planet.module.ts
@@ -5,9 +5,6 @@ import { TravelUser } from '../travel-user/travel-user.entity';
 import { Travel } from '../travel/travel.entity';
 import { User } from '../user/user.entity';
 import { PlanetController } from './api/v1/planet.controller';
-import { DirectPlanetPermissionGuard } from './guards/direct-planet-permission.guard';
-import { PlanetAccessGuard } from './guards/planet-access.guard';
-import { TimeRestrictionGuard } from './guards/time-restriction.guard';
 import { Planet } from './planet.entity';
 import { PlanetService } from './planet.service';
 
@@ -16,17 +13,7 @@ import { PlanetService } from './planet.service';
     TypeOrmModule.forFeature([Planet, Travel, User, TravelUser, PlanetUser]),
   ],
   controllers: [PlanetController],
-  providers: [
-    PlanetService,
-    PlanetAccessGuard,
-    DirectPlanetPermissionGuard,
-    TimeRestrictionGuard,
-  ],
-  exports: [
-    PlanetService,
-    PlanetAccessGuard,
-    DirectPlanetPermissionGuard,
-    TimeRestrictionGuard,
-  ],
+  providers: [PlanetService],
+  exports: [PlanetService],
 })
 export class PlanetModule {}
