9d087f47addd9716ec74caa743ff720e22628794
dev-jwshin <dev@foryourdev.com>
2025-08-13 10:42:47 +0900

    여행 모듈에서 TravelExpirySchedulerService 및 관련 유틸리티 삭제: 여행 만료 처리와 관련된 스케줄링 서비스 및 유틸리티를 제거하여 코드 간결성을 높임. TravelModule에서 해당 서비스의 제공 및 내보내기를 제거하고, 관련된 의존성을 정리함.

--- src/modules/travel/travel.module.ts
+++ src/modules/travel/travel.module.ts
@@ -5,14 +5,14 @@ import { TravelUser } from '../travel-user/travel-user.entity';
 import { User } from '../user/user.entity';
 import { TravelController } from './api/v1/travel.controller';
 import { TravelAccessGuard } from './guards/travel-access.guard';
-import { TravelExpirySchedulerService } from './services/travel-expiry-scheduler.service';
+
 import { Travel } from './travel.entity';
 import { TravelService } from './travel.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Travel, Planet, User, TravelUser])],
   controllers: [TravelController],
-  providers: [TravelService, TravelAccessGuard, TravelExpirySchedulerService],
-  exports: [TravelService, TravelAccessGuard, TravelExpirySchedulerService],
+  providers: [TravelService, TravelAccessGuard],
+  exports: [TravelService, TravelAccessGuard],
 })
 export class TravelModule {}
