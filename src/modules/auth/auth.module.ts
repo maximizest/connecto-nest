6b92dec0854a1a51a257be29054f3f0db1eabecc
dev-jwshin <dev@foryourdev.com>
2025-08-15 10:45:16 +0900

    회원탈퇴 기능 관련 서비스 및 엔티티 제거: UserDeletionService를 삭제하고, auth.module.ts에서 관련 엔티티들을 제거하여 코드 간결성을 높임. 회원탈퇴 기능을 더 이상 지원하지 않음.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,17 +2,8 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ENV_KEYS } from '../../common/constants/app.constants';
-import { Admin } from '../admin/admin.entity';
-import { FileUpload } from '../file-upload/file-upload.entity';
-import { Message } from '../message/message.entity';
-import { Notification } from '../notification/notification.entity';
-import { PlanetUser } from '../planet-user/planet-user.entity';
 import { Profile } from '../profile/profile.entity';
-import { MessageReadReceipt } from '../read-receipt/read-receipt.entity';
-import { TravelUser } from '../travel-user/travel-user.entity';
-import { UserDeletionService } from '../user/services/user-deletion.service';
 import { User } from '../user/user.entity';
-import { VideoProcessing } from '../video-processing/video-processing.entity';
 import { PushNotificationService } from '../notification/services/push-notification.service';
 import { AuthController } from './api/v1/auth.controller';
 import { AuthService } from './auth.service';
@@ -23,16 +14,6 @@ import { AuthService } from './auth.service';
       // Auth 관련 기본 entities
       User,
       Profile, // 프로필 자동 생성을 위해 필요
-
-      // UserDeletionService용 추가 entities (회원탈퇴 기능을 위해)
-      Admin,
-      Message,
-      Notification,
-      MessageReadReceipt,
-      FileUpload,
-      VideoProcessing,
-      TravelUser,
-      PlanetUser,
     ]),
 
     // JWT 모듈 설정
@@ -46,7 +27,6 @@ import { AuthService } from './auth.service';
   controllers: [AuthController],
   providers: [
     AuthService,
-    UserDeletionService, // 회원탈퇴 기능을 위해 포함
     PushNotificationService, // 로그인 시 푸시 토큰 등록
   ],
   exports: [
